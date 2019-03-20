470dc935a8c64eb80d13d7b4d3f14e6230698266
Justin Campbell <7772287+Faalqt@users.noreply.github.com>
2019-03-20 14:33:32 -0400

    Update scripts.php

--- PHP-Scripts/scripts.php
+++ PHP-Scripts/scripts.php
@@ -8,11 +8,11 @@
   }
 
   function buttonPress($stateID, $conn){
-      if(isset($_GET['add' . $stateID])){
-        updateDB('+', $stateID, $conn);
-      }else if(isset($_GET['sub'. $stateID])){
-        updateDB('-', $stateID, $conn);
-  }
+    if(isset($_GET['add' . $stateID])){
+      updateDB('+', $stateID, $conn);
+    }else if(isset($_GET['sub'. $stateID])){
+      updateDB('-', $stateID, $conn);
+    }
   }
 
   $choice = "stateSize DESC";
